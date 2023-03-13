1d797bbe31c423a50f561bfd2cc1f46c173ba097
KimJY <kjykjykjy500@naver.com>
2023-03-13 23:20:46 +0900

    HELLO MSG

---
+++ src/main/java/com/example/todaymenu/TodayMenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.todaymenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayMenuApplication.class, args);
+	}
+
+}
